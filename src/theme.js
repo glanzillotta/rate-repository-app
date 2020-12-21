4c1f4c158625a2a635fa8cb03795139faf36b081
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-12-21 11:05:20 +0100

    Exercise 10.10: a platform specific font

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,30 @@
+import { Platform } from 'react-native';
+
 const theme = {
-    colors: {
-      textPrimary: '#24292e',
-      textSecondary: '#FDFCFE',
-      textError: 'red',
-      primary: '#0165D4',
-      navBar: '#24292e',
-      backgroundMain: '#e1e4e8',
-      backgroundCard: 'white',
-    },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-    },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400',
-      bold: '700',
-    },
-  };
-  
-  export default theme;
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#FDFCFE',
+    textError: 'red',
+    primary: '#0165D4',
+    navBar: '#24292e',
+    backgroundMain: '#e1e4e8',
+    backgroundCard: 'white',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System'
+    }),
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
